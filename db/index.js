fbd0bd14c35612e3013f41e7c4136a5df0d90a38
j. jillian <judith.mediosdigitales@gmail.com>
2022-06-10 11:44:18 +0200

    Deploy test

--- db/index.js
+++ db/index.js
@@ -7,13 +7,12 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/surf-project";
 
+require('dotenv').config();
+
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+  .connect(process.env.MONGODB_URI)
+  .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+ 
+// ...
+
